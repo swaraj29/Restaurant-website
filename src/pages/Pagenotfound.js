2c55e7a043ae5d600a6008de29b2d0bb99237e73
swaraj kumar <028swarajkumar@gmail.com>
2023-10-06 16:09:08 +0530

    code added

---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+    </Layout>
+  )
+}
+
+export default Pagenotfound
